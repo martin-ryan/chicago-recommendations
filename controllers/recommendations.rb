4c6270c948ca30495a918be5055d8eb394b5da69
Martin Ryan <martinrryan@gmail.com>
2015-12-01 16:11:28 -0600

    sinatra server boilerplate

--- controllers/recommendations.rb
+++ controllers/recommendations.rb
@@ -1,6 +1,27 @@
 class RecommendationsController < ApplicationController
 
   get "/" do
+    @recommendations = Recommendation.all
+    erb :read_recommendation
+
+  end
+
+  get "/create" do
+
+    erb :create_recommendation
+
+  end
+
+  post "/create" do
+
+    p params
+
+    @rec = Recommendation.new
+    @rec.contributor = params[:contributor]
+    @rec.type_of_place = params[:type_of_place]
+    @rec.place = params[:place]
+    @rec.notes = params[:notes]
+    @rec.save
 
     erb :render
 
@@ -11,5 +32,11 @@ class RecommendationsController < ApplicationController
     Recommendation.all.to_json
 
   end
-  
+
+  get "/api/:whatever" do
+    puts params
+    return Recommendation.find(params[:whatever]).to_json
+  end
+
+
 end
